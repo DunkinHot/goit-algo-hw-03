dfae87f4eb8ea8cddd42878f5c3322410e10cd31
DunkinHot <vgaletsky@ukr.net  git config --global user.email>
2024-01-25 22:17:47 +0100

    second add

--- Task_2.py
+++ Task_2.py
@@ -2,12 +2,13 @@ import random
 #input range and number of instances
 min=1
 max=49
-quantity=6
+quantity=5
 
 def get_numbers_ticket(min, max, quantity):
-    if (max-min)<(quantity-1) or min>max or max>1000:
-        return "Invalid input values"
     try:
+        if (max-min)<(quantity-1) or min>max or max>1000:
+            print("Invalid input values")
+            return
         list=[]
         while len(list)<quantity:
             a=random.randint(min,max)
@@ -15,6 +16,7 @@ def get_numbers_ticket(min, max, quantity):
                 list.append(a)
         return(sorted(list))
     except:
-        return "Error input value"
+        print("Error input value")
+        return
     
 print(f"WINNING NUMBERS: {get_numbers_ticket(min, max, quantity)}")
