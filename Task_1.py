ade9bb66963759558bc5d2fe26d620e7d511050d
DunkinHot <vgaletsky@ukr.net  git config --global user.email>
2024-01-26 11:22:58 +0100

    Updated with Try/except

--- Task_1.py
+++ Task_1.py
@@ -5,7 +5,9 @@ import datetime
 date = "2020-10-09"
 from datetime import datetime as dtdt
 def get_days_from_today(date):
-    today = dtdt.now().date()
-    date_object = dtdt.strptime(date, '%Y-%m-%d').date()
+    try:
+        today = dtdt.now().date()
+        date_object = dtdt.strptime(date, '%Y-%m-%d').date()
+    except: return "Invalid data"
     return (today - date_object).days
 print(f"The difference is {get_days_from_today(date)} day(s)")
